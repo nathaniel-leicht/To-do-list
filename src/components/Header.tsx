20a36f77f7589aaa0ec0d58a8788ab7f2eae5251
Nadia <nadia.coelhoc@gmail.com>
2023-01-15 14:34:31 +0000

    Remove SVG icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import toDoLogo from '../assets/logo.svg'
+
 
 import styles from './Header.module.css'
 
 export function Header () {
     return (
         <header className={styles.header}>
-            <img src={toDoLogo} alt="Logo ToDo"/>
+
         </header>
     )
 }
